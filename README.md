5ac6971e95ad29297dac0fed8ff187f2f189c89b
mashile <sakkiemashile@gmail.com>
2020-08-06 13:42:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-zkalh4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-zkalh4)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './bootstrap.css';
+
+// Write javascript code
+const appDiv = 
+document.getElementById('app');
